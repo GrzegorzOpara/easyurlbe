37c19f8b8405448d020a2f7c180db66ec7a5dbfc
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-04 19:20:28 +0100

    get and put methods implemented

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from .views import (
     UrlListView,
-    UrlDetailView
+    UrlDetailView,
+    UserListView
 )
 
 urlpatterns = [
+    path('users/', UserListView.as_view()),
     path('urls/', UrlListView.as_view()),
     path('urls/<int:url_id>/', UrlDetailView.as_view()),
 ]
