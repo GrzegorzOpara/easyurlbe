a73bbbf765d01a3b0cbc473ba30a6619310bd039
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-07 09:44:48 +0100

    fixing CORS

--- easyurlbe/settings.py
+++ easyurlbe/settings.py
@@ -90,17 +90,12 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # my middleware
-
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-
 ]
 
 REST_FRAMEWORK = {
