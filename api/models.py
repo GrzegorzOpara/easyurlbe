ad2951ef6e81a641f4b444859bc81e0ff551a618
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-11 18:15:00 +0100

    chnaged data type for url field

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UrlEntry(models.Model):
     url_name = models.CharField(max_length = 200)
-    url_link = models.URLField()
+    url_link = models.TextField()
     url_desc = models.TextField(null = True, blank = True)
     user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
 
