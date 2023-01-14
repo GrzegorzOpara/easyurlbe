a978e38b9b7e6f48312638f196654fba6be470c8
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-14 19:07:57 +0100

    fixing bug with missing data in return statement

--- api/serializers.py
+++ api/serializers.py
@@ -44,17 +44,23 @@ class ResetPasswordSerializer(serializers.Serializer):
         Verify token and encoded_pk and then set new password.
         """
         password = data.get("password")
+        print(password)
         token = self.context.get("kwargs").get("token")
+        print(token)
         encoded_pk = self.context.get("kwargs").get("encoded_pk")
+        print(encoded_pk)
 
         if token is None or encoded_pk is None:
             raise serializers.ValidationError("Missing data.")
 
         pk = urlsafe_base64_decode(encoded_pk).decode()
+        print(pk)
         user = User.objects.get(pk=pk)
+        print(user)
+
         if not PasswordResetTokenGenerator().check_token(user, token):
             raise serializers.ValidationError("The reset token is invalid")
 
         user.set_password(password)
         user.save()
-        return 
+        return data
