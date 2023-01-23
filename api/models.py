950a7a6b20a5eab9cb7532817d09d3a99bf88101
Grzegorz Opara <grzegorz.opara@gmail.com>
2023-01-23 16:37:12 +0100

    added Profile model that extends User model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -11,3 +13,17 @@ class UrlEntry(models.Model):
 
     def __str__(self) -> str:
         return self.url_name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar_link = models.TextField()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
